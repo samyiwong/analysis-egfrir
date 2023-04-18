9a7531866e11cb22a18adf3da8b2063bfda1e7b5
David J. H. Shih <djh.shih@gmail.com>
2023-04-18 18:16:25 +0800

    Output expression and analysis data.
    Map probe ids to ensembl ids and gene symbols using
    Bioconductor annotation package.

--- GSE49135/analyze.R
+++ GSE49135/analyze.R
@@ -1,25 +1,39 @@
 library(GEOquery)
 library(limma)
+library(io)
+
+# platform: GPL10558
+#           Illumina HumanHT-12 V4.0 expression beadchip
+library(illuminaHumanv4.db)
+db <- illuminaHumanv4.db;
 
 source("../R/common.R");
 
-gse <- getGEO("GSE49135")[[1]];
+accession <- "GSE49135";
+out.fn <- filename("GSE49135");
+
+gse <- getGEO(accession)[[1]];
 
 x <- exprs(gse);
+
+# examine distribution of expression data
 hist(x, breaks=100);
 summary(x)
 min(x)
 
-# left-shift towards 0
+# left-shift towards 0 because x has negative values
 x <- x - min(x);
 hist(x, breaks=100);
 
-
 x.f <- filter_undetected(x);
+
+# skip filtering undetected genes
+#x.f <- x;
 
 dim(x)
 dim(x.f)
 
+# examine distribution of pre-processed data
 hist(x.f, breaks=100);
 boxplot(x.f)
 max(x.f)
@@ -36,14 +50,33 @@ fit <- lmFit(x.f, design);
 fit <- eBayes(fit);
 res <- topTable(fit, coef="phenotypeerlotinib-resistant", number=Inf);
 
-idx <- match(rownames(res), pData(featureData(gse))$ID);
-res$gene <- pData(featureData(gse))[idx, "Symbol"];
+# map probe id to gene symbol
+res$gene <- mapIds(
+	db, keys=rownames(res), keytype="PROBEID",
+	column="SYMBOL"
+);
 
-res[res$gene == "AXL", ]
+# map probe id to ensembl id
+res$ensembl <- mapIds(
+	db, keys=rownames(res), keytype="PROBEID",
+	column="ENSEMBL"
+);
 
-head(res, 100)
-pheno
-x.f["ILMN_1666733", ]
-x.f["ILMN_2181593", ]
-x.f[c("ILMN_2192072", "ILMN_1685403"), ]
+# construct feature data
+probes <- rownames(x.f);
+features <- data.frame(
+	row.names = probes,
+	ensembl = mapIds(db, keys=probes, keytype="PROBEID", column="ENSEMBL"),
+	gene = mapIds(db, keys=probes, keytype="PROBEID", column="SYMBOL")
+);
+
+gse2 <- ExpressionSet(x.f,
+	phenoData = AnnotatedDataFrame(pheno),
+	featureData = AnnotatedDataFrame(features),
+	experimentData = experimentData(gse),
+	annotation = annotation(gse)
+);
+
+qwrite(res, insert(out.fn, tag="limma", ext="rds"));
+qwrite(gse2, insert(out.fn, tag="eset", ext="rds"));
 
